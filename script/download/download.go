017142d6c54784ab99f213b22ab01deb456d5109
Michael Bryzek <mbryzek@alum.mit.edu>
2017-02-14 17:57:53 -0500

    Write csv to json

--- script/download/download.go
+++ script/download/download.go
@@ -3,7 +3,7 @@ package download
 // Downloads source files, storing locally in the sources directory
 
 import (
-	"./common"
+//	"../common"
 	"encoding/csv"
 	"encoding/json"
         "fmt"
@@ -11,34 +11,60 @@ import (
         "io/ioutil"
         "net/http"
         "os"
-
+        "strings"
 )
 
-type Currency struct {
-	Iso4217        string
-	Name           string
-	NumberDecimal  string
+type Continent struct {
+	Name       string `json:"name"`
+	Code       string `json:"code"`
+	Countries  []string `json:"countries"`
 }
 
+type Country struct {
+	Name                     string `json:"name"`
+	Continent                string `json:"continent"`
+	Iso_3166_2               string `json:"iso_3166_2"`
+	Iso_3166_3               string `json:"iso_3166_3"`
+	CurrencyCode             string `json:"currency_code"`
+	CurrencyName             string `json:"currency_name"`
+	CurrencyNumberDecimals   string `json:"currency_number_decimals"`
+	Languages                []string `json:"languages"`
+}
+
+type Currency struct {
+	Name                     string `json:"name"`
+	Iso_4217_3               string `json:"iso_4217_3"`
+	NumberDecimals           int    `json:"number_decimals"`
+}
+
+type Language struct {
+	Name                     string `json:"name"`
+	Iso_639_2                string `json:"iso_639_2"`
+	Countries                []string `json:"countries"`
+}
+
+type convert func(records map[string]string) interface{}
 
 func DownloadAll() {
 	// tmp := download("https://raw.githubusercontent.com/bdswiss/country-language/master/data.json")
 	// os.Rename(tmp, "sources/languages.json")
 
-	tmp := csvToJson(
-		download("https://raw.githubusercontent.com/datasets/country-codes/master/data/country-codes.csv"),
-		map[string]string{
-			"official_name_en": "name",
-			"ISO3166-1-Alpha-2": "iso31662",
-			"ISO3166-1-Alpha-3": "iso31663",
-			"Languages": "languages",
-			"ISO4217-currency_alphabetic_code": "currency_code",
-			"ISO4217-currency_minor_unit": "currency_number_decimals",
-			"ISO4217-currency_name": "currency_name",
-			"Continent": "continent",
-		},
-	)
-	os.Rename(tmp, "sources/countries.json")
+	tmp := download("https://raw.githubusercontent.com/datasets/country-codes/master/data/country-codes.csv")
+	countries := readCsv(tmp)
+	writeJson("sources/countries.json", toObjects(countries, func(record map[string]string) interface{} {
+		return Country {
+			Name: record["official_name_en"],
+			Iso_3166_2: record["ISO3166-1-Alpha-2"],
+			Iso_3166_3: record["ISO3166-1-Alpha-3"],
+			CurrencyCode: record["ISO4217-currency_alphabetic_code"],
+			CurrencyNumberDecimals: record["ISO4217-currency_number_decimals"],
+			CurrencyName: record["ISO4217-currency_name"],
+			Languages: strings.Split(record["Languages"], ","),
+			Continent: record["Continent"],
+		}
+	}))
+	fmt.Println(len(countries))
+	//os.Rename(tmp, "sources/countries.json")
 
 	// tmp = download("http://dev.maxmind.com/static/csv/codes/country_continent.csv")
 	// os.Rename(tmp, "sources/continents.csv")
@@ -68,20 +94,17 @@ func download(url string) string {
 	return target.Name()
 }
 
-// csvToJson reads the CSV data from the specified file, writing to a file in
-// json format. Returns a path to the json file
-func csvToJson(file string, nameMap map[string]string) string {
-	jsonFile, err := ioutil.TempFile("", "reference-csv-to-json")
-	exitIfError(err, "Error creating temporary file")
-	defer jsonFile.Close()
-	enc := json.NewEncoder(jsonFile)
-
+// readCsv Reads a CSV file, mapping names to well known values, and returns
+// a list of map[string]string objects
+func readCsv(file string) []map[string]string {
 	input, err := os.Open(file)
 	exitIfError(err, fmt.Sprintf("Error opening file %s", file))
 
         r := csv.NewReader(input)
 	var headers []string
 	first := true
+	var all []map[string]string
+
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -92,22 +115,38 @@ func csvToJson(file string, nameMap map[string]string) string {
 			headers = record
 			first = false
 		} else {
-			fmt.Println(record)
-			json := make(map[string]interface{})
+			row := make(map[string]string)
 
 			for i, v := range record {
 				if (v != "") {
-					header := nameMap[headers[i]]
-					if (header != "") {
-						json[header] = v
-					}
+					row[headers[i]] = v
 				}
 			}
-			fmt.Println(json)
-			err := enc.Encode(&json)
-			exitIfError(err, "Error marshalling record to json")
+			all = append(all, row)
 		}
 	}
 
-	return jsonFile.Name()
+	return all
 }
+
+func writeJson(target string, data interface{}) {
+	jsonFile, err := ioutil.TempFile("", "reference-csv-to-json")
+	exitIfError(err, "Error creating temporary file")
+	defer jsonFile.Close()
+
+	enc := json.NewEncoder(jsonFile)
+	err = enc.Encode(&data)
+	exitIfError(err, "Error marshalling record to json")
+
+	err = os.Rename(jsonFile.Name(), target)
+	exitIfError(err, "Error renaming tmp file")
+}
+
+func toObjects(records []map[string]string, f convert) []interface{} {
+	var all []interface{}
+	for _, v := range records {
+		all = append(all, f(v))
+	}
+	return all
+}
+
