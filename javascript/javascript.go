8ff7ec7b7264a8692e3452a9fc541683e29fc53d
Michael Bryzek <mbryzek@alum.mit.edu>
2017-03-28 11:24:30 -0400

    Introduce formats

--- javascript/javascript.go
+++ javascript/javascript.go
@@ -2,25 +2,26 @@ package javascript
 
 import (
 	"../common"
+	"errors"
 	"fmt"
 	"os"
 )
 
 type CurrencyFormat struct {
 	Formats      map[string]JavascriptFormat `json:"formats"`
-	Locales      map[string]LocaleDicionary  `json:"locales"`
+	Locales      map[string]LocaleDictionary  `json:"locales"`
 }
 
 type JavascriptFormat struct {
 	Symbol     string `json:"symbol"`
 	Decimal    string `json:"decimal"`
 	Group      string `json:"group"`
-	Precision  string `json:"precision"`
+	Precision  int    `json:"precision"`
 	Format     string `json:"format"`
 }
 
-type LocaleDicionary struct {
-	Locale     string `json:"locale"`
+type LocaleDictionary struct {
+	Format     string `json:"format"`
 }
 
 type CommonData struct {
@@ -47,23 +48,60 @@ func generateFormats(data CommonData) map[string]JavascriptFormat {
 	all := map[string]JavascriptFormat{}
 
 	for _, l := range data.Locales {
-		all[l.Id] = JavascriptFormat{
-			Symbol: l.Symbol,
-			Decimal: l.Numbers.Decimal,
-			Group: l.Numbers.Group,
-			Precision: 2,
-			Format: "%s%v",
+		currency, err := findCurrencyByLocale(data, l)
+		if err != nil {
+			all[l.Id] = JavascriptFormat{
+				Symbol: currency.Symbols.Primary,
+				Decimal: l.Numbers.Decimal,
+				Group: l.Numbers.Group,
+				Precision: currency.NumberDecimals,
+				Format: "%s%v",
+			}
 		}
 	}
 
 	return all;
 }
 
-func generateLocaleDictionary(data CommonData) map[string]LocaleDicionary {
-	all := map[string]LocaleDicionary{}
+func generateLocaleDictionary(data CommonData) map[string]LocaleDictionary {
+	all := map[string]LocaleDictionary{}
 	for _, l := range data.Locales {
-		all[l.Id] = "1"
+		all[l.Id] = LocaleDictionary{
+			Format: "1",
+		}
 	}
 	return all
 }
 
+func findCurrencyByLocale(data CommonData, locale common.Locale) (common.Currency, error) {
+	country := findCountryByIso31663(data, locale.Country)
+	if country.DefaultCurrency == "" {
+		return common.Currency{}, errors.New("Country has no default currency")
+	} else {
+		return findCurrencyByIso42173(data, country.DefaultCurrency), nil
+	}
+}
+
+func findCountryByIso31663(data CommonData, country string) common.Country {
+	for _, c := range data.Countries {
+		if c.Iso_3166_3 == country {
+			return c
+		}
+	}
+
+	fmt.Printf("ERROR: Country[%s] not found\n", country)
+	os.Exit(1)
+	return common.Country{}
+}
+
+func findCurrencyByIso42173(data CommonData, currency string) common.Currency {
+	for _, c := range data.Currencies {
+		if c.Iso_4217_3 == currency {
+			return c
+		}
+	}
+
+	fmt.Printf("ERROR: Currency[%s] not found\n", currency)
+	os.Exit(1)
+	return common.Currency{}
+}
